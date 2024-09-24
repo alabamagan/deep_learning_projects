3282985e07c5fac093585ddf3f5fa6d9dc302aaf
lwong <fromosia@gmail.com>
2024-09-24 15:54:52 +0800

    Refactor DICOM processing functions
    - Replace `Dcm2NiiConverter` with `batch_dicom2nii` for batch processing.
    - Add parameters `id_globber` and `num_workers` to `flatten_dcm`.
    - Implement `make_datasheet` to create a DataFrame from NIfTI files.
    - Update unit tests to cover new functionalities and multiprocessing.
    - Fix shape handling for ground truth in `rAIdiologistInferencer`.

--- SequenceIdentifier/sequence_identifier/io/preprocessing.py
+++ SequenceIdentifier/sequence_identifier/io/preprocessing.py
@@ -1,15 +1,24 @@
 import SimpleITK as sitk
 import numpy as np
 import re
+
 from pathlib import Path
 from typing import List, Tuple, Optional, Dict
-from mnts.io.data_formatting import Dcm2NiiConverter
+
+import pandas as pd
+
+from mnts.io.data_formatting import batch_dicom2nii
 from mnts.io import dicom2nii, pydicom_read_series
 from mnts.mnts_logger import MNTSLogger
+from mnts.utils.sequence_check import unify_mri_sequence_name
+
 import tempfile
 import pprint
 
-def flatten_dcm(folder: Path) -> Dict[str, sitk.Image]:
+
+def flatten_dcm(folder: Path,
+                id_globber: Optional[str] = r'[A-Za-z]*[0-9]+',
+                num_workers: Optional[int] = 1) -> Dict[str, sitk.Image]:
     """
     Converts DICOM series in a folder to flattened NIfTI images.
 
@@ -21,6 +30,10 @@ def flatten_dcm(folder: Path) -> Dict[str, sitk.Image]:
     Args:
         folder (Path):
             Path to the folder containing DICOM series to be processed.
+        id_globber (Optional[str]):
+            Optional regex pattern to match IDs from the filenames. Defaults
+            to r'[A-Za-z]*[0-9]+'.
+
 
     Returns:
         Dict[str, sitk.Image]:
@@ -31,9 +44,7 @@ def flatten_dcm(folder: Path) -> Dict[str, sitk.Image]:
         FileNotFoundError: If the specified folder does not exist.
         RuntimeError: If an error occurs during DICOM to NIfTI conversion.
     """
-
     output = {}
-    id_globber = '\w*\d+'
     with (tempfile.TemporaryDirectory() as tmp_dir,
         MNTSLogger('flatten_dcm') as logger):
         logger.info(f"Working on {str(folder)}")
@@ -42,17 +53,17 @@ def flatten_dcm(folder: Path) -> Dict[str, sitk.Image]:
         logger.info(f"Identified series: {pprint.pformat(series.keys())}")
         logger.info("Converting to NII")
 
-        for series_id in series:
-            converter = Dcm2NiiConverter(
-                series[series_id][0].parent,
-                tmp_dir,
-                idglobber=id_globber,
-                check_im_type=True,
-                use_patient_id=True,
-                add_scan_time=False,
-                dump_meta_data=True
-            )
-            converter.Execute()
+        batch_dicom2nii(
+            [pathlist[0].parent for pathlist in series.values()],
+            tmp_dir,
+            workers=num_workers,
+            idglobber=id_globber,
+            check_im_type=True,
+            use_patient_id=True,
+            add_scan_time=False,
+            dump_meta_data=True
+        )
+
 
         # Iterate over all .nii.gz files in the specified folder
         for nii_file in Path(tmp_dir).rglob("*.nii.gz"):
@@ -78,4 +89,53 @@ def flatten_dcm(folder: Path) -> Dict[str, sitk.Image]:
     return output
 
 
+def make_datasheet(folder: Path, id_globber: Optional[str] = r'[A-Za-z]*[0-9]+') -> pd.DataFrame:
+    """Creates a datasheet from NIfTI files in a folder.
 
+    Iterates over NIfTI files in the specified folder, extracting
+    information and compiling it into a pandas DataFrame.
+
+    Args:
+        folder (Path):
+            Path to the folder containing NIfTI files.
+        id_globber (Optional[str]):
+            Regular expression pattern to extract IDs from filenames.
+            Defaults to r'[A-Za-z]*[0-9]+'
+
+    Returns:
+        pd.DataFrame:
+            A DataFrame where each column represents metadata extracted
+            from a NIfTI file, with IDs and unified MRI sequence names.
+            Each row is the file processed.
+
+    Raises:
+        ValueError: If no files match the specified pattern.
+    """
+    # Find all NIfTI files in the folder
+    nii_files = folder.rglob("*.nii.gz")
+
+    # Initialize a list to store each data row
+    rows = []
+
+    # Iterate over each file in the folder
+    for r in folder.iterdir():
+        # Extract the ID from the filename using a regular expression
+        id = re.search(id_globber, r.name).group()
+
+        # Get a dictionary of MRI sequence information
+        _, row_dict = unify_mri_sequence_name(r.name, return_glob_dict=True, glob_techniques=True)
+
+        # Create a pandas Series from the dictionary, naming it after the file
+        row = pd.Series(row_dict, name=r.name)
+
+        # Add the extracted ID to the Series
+        row['Study Number'] = id
+
+        # Append the Series to the list of rows
+        rows.append(row)
+
+    # Concatenate all data rows into a single DataFrame, rows are filenames
+    df_out = pd.concat(rows, axis=1).T
+    df_out.sort_index(inplace=True)
+    df_out.sort_index(axis=1, inplace=True)
+    return df_out.infer_objects()
